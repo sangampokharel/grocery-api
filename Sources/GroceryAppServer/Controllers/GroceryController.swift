fbacea903d64bd1962cce0788f197a111147678a
sangam <sangampokharel803@gmail.com>
2025-09-22 14:34:03 +0545

    main: Implemented the delete api for categories

--- Sources/GroceryAppServer/Controllers/GroceryController.swift
+++ Sources/GroceryAppServer/Controllers/GroceryController.swift
@@ -17,17 +17,24 @@ class GroceryController:RouteCollection, @unchecked Sendable {
         let api = routes.grouped("api","users",":userId")
         
         
-            //POST: Saving GroceryCategory
-            // /api/users/:userId/grocery-categories
+        //POST: Saving GroceryCategory
+        // /api/users/:userId/grocery-categories
         api.post("grocery-categories", use: saveGroceryCategories)
-            
+        
+        //GET: TO get the list of categories of particular users
+        api.get("grocery-categories",use:getGroceryCategoriesByUser)
+        
+        //Delete: Delete grocery
+        // /api/users/:userId/grocery-categories/:groceryId
+        api.delete("grocery-categories", ":groceryCategoryId", use: deleteGroceryCategory)
+        
     }
     
     func saveGroceryCategories(req:Request) async throws -> GroceryCategoryResponseDTO {
         
         // validate the request DTO instead of the model
         try GroceryCategoryRequestDTO.validate(content: req)
-       
+        
         //decode
         guard let userId = req.parameters.get("userId",as:UUID.self) else {
             throw Abort(.notFound)
@@ -42,5 +49,44 @@ class GroceryController:RouteCollection, @unchecked Sendable {
         
         // response DTO
         return try GroceryCategoryResponseDTO(id: groceryCategory.requireID(), title: groceryCategory.title, colorCode: groceryCategory.colorCode)
+    }
+    
+    
+    func deleteGroceryCategory(req:Request) async throws -> GroceryCategoryResponseDTO {
+        
+       
+        guard let userId = req.parameters.get("userId",as:UUID.self),
+              let groceryId = req.parameters.get("groceryCategoryId",as:UUID.self) else {
+            throw Abort(.notFound)
+        }
+      
+       guard let groceryCategory = try await GroceryCategory.query(on: req.db)
+            .filter(\.$user.$id == userId)
+            .filter(\.$id == groceryId)
+            .first() else {
+           
+           throw Abort(.notFound)
+       }
+        
+        try await groceryCategory.delete(on: req.db)
+        
+        return GroceryCategoryResponseDTO(id: try groceryCategory.requireID(), title: groceryCategory.title, colorCode: groceryCategory.colorCode)
+        
+    }
+    
+    func getGroceryCategoriesByUser(req:Request) async throws -> [GroceryCategoryResponseDTO] {
+        // get the user id
+        guard let userId = req.parameters.get("userId",as:UUID.self) else {
+            throw Abort(.notFound)
+        }
+        
+        // go in grocerries categegories databse and get the categories of particular user
+        let groceries = try await GroceryCategory.query(on: req.db).filter(\.$user.$id == userId)
+            .all()
+            .compactMap { try GroceryCategoryResponseDTO(id: $0.requireID(), title: $0.title, colorCode: $0.colorCode)}
+        
+        // return the json
+        
+        return groceries
     }
 }
